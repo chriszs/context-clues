677d197e496970d30d64e19a96d97c9bab5948e4
Chris Zubak-Skees <chriszs@gmail.com>
2012-11-16 04:42:25 -0500

    rearranged and debugged wikiPage

--- wikiPage.js
+++ wikiPage.js
@@ -1,85 +1,150 @@
+var events = require("events");
+
+var emitter = new events.EventEmitter();
+
+var queue = new Array();
+
+var currentp = null;
+
+exports.getWorking = function () {
+	var count = 0;
+	for (i in queue) {
+		count++;
+	}
+	return count;
+}
+
+exports.on = function (event, listener) {
+	emitter.on(event, listener);
+}
+
+var handleProcessed = function (p) {
+	if (currentp == p) {
+		currentp = queue.shift();
+		currentp.start();
+	}
+}
+
+exports.on("processed",handleProcessed);
+
 exports.wikiPage = function (db,title) {
-	// console.log("new "+title);
 	var links = new Array();
 	var verified = false;
 	var callback = function (n) {
-		//	console.log("called empty function");
 	};
 	var node = null;
 	var me = this;
+	var relationsLeft = 0;
 	
-	this.save = function (c) {
-	//	console.log("save "+title);
-		if (typeof c == "function") {
-			callback = c;
-		}
-		db.getIndexedNode("pages","title",title,me.setupNode);
+	this.getTitle = function () {
+		return title;
 	}
 	
 	this.setVerified = function (v) {
 		verified = v;
 	}
 	
-	this.relationSaved = function (err,result) {
-		console.log("relationSaved "+title);
-		if (err) {
-			console.log("problem saving relation");
+	this.addLink = function (page,text) {
+		var parts = page.split(":");
+		if (parts.length == 1 ) {
+			var p = new exports.wikiPage(db,page);
+			links.push(p);
 		}
 	}
 	
-	this.saveRelation = function (n) {
-		// console.log(node.id + "->" + n.id);
-		node.createRelationshipTo(n,"link",{},me.relationSaved);
-	}
-	
-	this.saveLinks = function () {
-	//	console.log("saveLinks "+title);
-		for (i in links) {
-			links[i].save(me.saveRelation);
+	this.queue = function () {
+		if (verified && currentp == null) {
+			currentp = me;
+			me.save(callback);
+		}
+		else if (verified) {
+			queue.push(this);
 		}
 	}
 	
-	this.nodeSaved = function (err,n) {
-	//	console.log("nodeSaved "+title);
-		if (err) {
-			console.log("problem saving node:" + err);
+	this.save = function (c) {
+		if (typeof c == "function") {
+			callback = c;
 		}
-		else {
-			// console.log(n);
-			node = n;
-			callback(n);
-			// console.log("save links called");
-			// console.log(this);
-			me.saveLinks();
-		}
+		me.start();
 	}
 	
-	this.setupNode = function (err, n) {
-		// console.log("setupNode "+title);
-		
+	this.start = function () {
+		db.getIndexedNode("pages","title",title,me.retrieveNode);
+	}
+	
+	this.retrieveNode = function (err, n) {
 		if (err || n == null) {
 			n = db.createNode({"title":title,"verified":verified});
 			
 			n.save(me.nodeSaved);
 		}
 		else {
-			// console.log(n);
 			node = n;
 			callback(n);
-			// console.log("save links called");
-			// console.log(this);
-			saveLinks();
+			me.saveLinks();
+			me.updateNode();
 		}
 	}
 	
-	this.addLink = function (page,text) {
-		// console.log("addLink "+title);
-		
-		var parts = page.split(":");
-		if (parts.length == 1 ) {
-			// console.log(title+"=>"+page);
-			var p = new exports.wikiPage(db,page);
-			links.push(p);
+	this.nodeSaved = function (err,n) {
+		if (err) {
+			console.log("problem saving node:" + err);
+		}
+		else {
+			console.log("created "+n.data.title);
+			node = n;
+			n.index("pages","title",title,me.nodeIndexed);
+		}
+	}
+	
+	this.nodeIndexed = function (err,result) {
+		if (err) {
+			console.log("problem saving node:" + err);
+		}
+		else {
+			me.saveLinks();
+			callback(node);
+		}
+	}
+	
+	this.saveLinks = function () {
+		relationsLeft = links.length;
+		for (i in links) {
+			links[i].save(me.saveRelation);
+		}
+		if (links.length == 0) {
+			emitter.emit("processed",me);
+		}
+	}
+	
+	this.saveRelation = function (n) {
+		node.createRelationshipTo(n,"link",{},me.relationSaved);
+	}
+	
+	this.relationSaved = function (err,r) {
+		if (err) {
+			console.log("problem saving relation:"+err);
+		}
+		else {
+			console.log(r.start.data.title + "->" + r.end.data.title);
+		}
+		relationsLeft--;
+		if (relationsLeft == 0) {
+			emitter.emit("processed",me);
+		}
+	}
+	
+	this.updateNode = function () {
+		if (verified && !node.data.verified) {
+			node.data.verified = true;
+			node.save(me.nodeUpdated);
+		}
+	}
+	
+	this.nodeUpdated = function (err,result) {
+		if (err) {
+			console.log("problem saving node:" + err);
 		}
 	}
 }
